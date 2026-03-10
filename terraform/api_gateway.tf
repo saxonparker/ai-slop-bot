311f074052d91968092ef3bc870cc9fda77f089b
Saxon Parker <parker.saxon@gmail.com>
2026-03-10 11:52:53 -0600

    Fix base64 encoding handling

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -13,7 +13,7 @@ resource "aws_apigatewayv2_integration" "dispatch" {
   api_id                 = aws_apigatewayv2_api.ai_slop.id
   integration_type       = "AWS_PROXY"
   integration_uri        = aws_lambda_function.dispatch.invoke_arn
-  payload_format_version = "2.0"
+  payload_format_version = "1.0"
 }
 
 resource "aws_apigatewayv2_route" "post_ai_slop" {
